41bb84b622a7fc7c545e2c93524b191eb5001092
MariusB5 <marius.briscut5@gmail.com>
2022-10-03 14:25:52 -0400

    some simplifications

--- playing_banjo/playing_banjo.py
+++ playing_banjo/playing_banjo.py
@@ -1,7 +1,6 @@
 def are_you_playing_banjo(name):
-    list1 = []
-    list1[:0] = name
-    if name[0] in ["r", "R"]:
+    list1 = list(name)
+    if list1[0] in ["r", "R"]:
         conclusion = name + " plays banjo"
     else:
         conclusion = name + " does not play banjo"
