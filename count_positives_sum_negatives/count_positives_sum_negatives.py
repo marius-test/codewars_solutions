f9ac1b61dd17394d693dd2491140e898406f39c3
MariusB5 <marius.briscut5@gmail.com>
2022-10-07 12:15:58 -0400

    count_positives_sum_negatives

--- count_positives_sum_negatives/count_positives_sum_negatives.py
+++ count_positives_sum_negatives/count_positives_sum_negatives.py
@@ -1,2 +1,6 @@
 def count_positives_sum_negatives(arr):
-    return
+    if len(arr) == 0:
+        return []
+    positive_count = sum([1 for i in arr if i > 0])
+    negative_sum = sum([i for i in arr if i < 0])
+    return [positive_count, negative_sum]
