fdbaf377c57727bc9fa89ee856268bc3df7c1a17
Marius Briscut <marius.briscut5@gmail.com>
2025-10-20 08:05:12 +0200

    coming back from a short pause

--- debug.py
+++ debug.py
@@ -1,19 +1,10 @@
 # for testing and debugging code
-import re
-
-text1 = "the-stealth-warrior"
 
 
-def to_camel_case(text):
-    words = re.split("[-_]", text)
-    result = [words[0]]
-    for w in words[1:]:
-        result.append(w.capitalize())
-    camel = "".join(result)
-        
-    return camel
+def dna_to_rna(dna):
+    return
 
 
 if __name__ == "__main__":
-    print(to_camel_case(text1))
+    print("It works!")
     
